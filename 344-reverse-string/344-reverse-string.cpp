8710fe83e9eb0e9af57804d8ffec90d63a3f2762
CodewithShivali <93459768+CodewithShivali@users.noreply.github.com>
2022-08-06 23:59:00 +0530

    Time: 42 ms (32.58%), Space: 23.2 MB (40.83%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     void reverseString(vector<char>& s) {
         int temp;
@@ -7,6 +7,23 @@ public:
          temp=s[i];
          s[i]=s[s.size()-1-i];
          s[s.size()-1-i]=temp;
+        }
+    }
+};*/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        int right = s.size()-1;
+        int left = 0;
+        while(left < right)
+        {
+            char c = s[left];
+            s[left] = s[right];
+            s[right] = c;
+            
+            left+=1;
+            right-=1;
         }
     }
 };
